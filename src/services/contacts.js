2dde016cf52a46c8025dec707c67b5277c838c40
1ster14ka <danyazybastik@gmail.com>
2025-03-08 10:25:02 +0200

    update data base: add method patch, delete

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,14 +1,25 @@
 import Contacts from '../models/contacts.js';
 
 export async function getAllContacts() {
-  try {
-    return await Contacts.find();
-  } catch (error) {
-    console.error('Ошибка при получении контактов:', error);
-  }
+  const contacts = await Contacts.find();
+
+  return contacts;
 }
 
 export async function getContactById(id) {
   const contact = await Contacts.findById(id);
   return contact;
+}
+
+export async function createContact(payload) {
+  const contact = await Contacts.create(payload);
+  return contact;
+}
+
+export async function updateContact(contactId, contact) {
+  return await Contacts.findByIdAndUpdate(contactId, contact, { new: true });
+}
+
+export async function deleteContact(contactId) {
+  return await Contacts.findByIdAndDelete(contactId);
 }
